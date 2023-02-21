4c3e74e8c284a31add425e4f2ef923b5ef2f8448
Twixxik <twixxik@ya.ru>
2023-02-21 11:17:43 +0300

    correct upload backup

--- bxdump-upload.sh
+++ bxdump-upload.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tar -czf ~/bxdump/bxdump-upload.tar.gz -X ~/bxdump/.ignore.upload --no-recursion --files-from <(find ./upload -size -10M -o -type d)
+tar -czf ~/bxdump/bxdump-upload.tar.gz -X ~/bxdump/.ignore.upload upload
